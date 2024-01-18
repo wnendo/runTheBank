1c9f243672263f66483594407faee53042fdad06
Wendel <wnendo@gmail.com>
2024-01-18 18:22:42 -0300

    Creating doc Creating some unit tests

--- src/main/java/com/wendel/test/runTheBank/adapter/controller/AccountController.java
+++ src/main/java/com/wendel/test/runTheBank/adapter/controller/AccountController.java
@@ -1,15 +1,9 @@
 package com.wendel.test.runTheBank.adapter.controller;
 
 import com.wendel.test.runTheBank.adapter.controller.request.AccountRequest;
-import com.wendel.test.runTheBank.adapter.controller.request.RegisterRequest;
 import com.wendel.test.runTheBank.adapter.controller.response.AccountResponse;
-import com.wendel.test.runTheBank.adapter.controller.response.RegisterResponse;
-import com.wendel.test.runTheBank.domain.Account;
 import com.wendel.test.runTheBank.usecase.account.CreateAccount;
 import com.wendel.test.runTheBank.usecase.account.GetAccount;
-import com.wendel.test.runTheBank.usecase.register.CreateRegister;
-import com.wendel.test.runTheBank.usecase.register.GetRegister;
-import io.unlogged.Unlogged;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
